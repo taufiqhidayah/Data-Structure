a5eaba873205c13ffd464d3240f8fb1ea70c6345
taufiqhidayah <taufiqhidayaht@gmail.com>
2018-03-15 08:43:11 +0700

    Kedua

--- 2 bluerice.cpp
+++ 2 bluerice.cpp
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAXSIZE 5
-struct Beras {
-	char name[19];
-  	int stock[MAXSIZE];
-	int top;
-};
-typedef struct Beras STACK;
-STACK s;
- 
-//struct Beras Products[10];
-//Beras sa;
+typedef struct{
+  char name[19];
+  int stk[MAXSIZE];
+  int top;
+}mhs;
+typedef struct stack STACK;
+mhs student[100];
 
 char riceType[98];
 
@@ -22,59 +19,48 @@ void stockRice();
 
 void initCake();
 void insertRice();
-
 int main (){
-	
-  initCake();
+  //  initCake();
+  student[0].top = -1;
   getMenu();	
   return 0;
 }
 
 void insertRice(){
-  	 int num;
-    if (s.top == (MAXSIZE - 1))
-    {
-        printf ("Stack is Full\n");
-        return;
-    }
-    else
-    {
-    	char namea[12];
-		
-//		while (strcmp (name,"long")  == 0  && strcmp (name,"medium")  == 0  && strcmp (name,"short")  == 0  ){
-//			printf("Input Rice type [ long / medium / short ]");
-//			scanf("%s"s.name);
-//		}
-        printf ("Enter the element to be pushed\n");
-        scanf ("%d", &num);
-        printf("Input Rice type [ long / medium / short ]");
-		scanf("%s", s.name);
-        s.top = s.top + 1;
-        s.stock[s.top] = num;
-    
-    }
- 
-//  scanf("%s", riceType);
-//  while(strcmp (riceType,"long")  != 0 || strcmp (riceType,"medium" ) != 0 ||strcmp(riceType, "short" ) != 0 ){
-//    printf("Input Rice type [ long / medium / short ]");
-//    scanf("%s", riceType);	
-//  }
-
-     system("pause");
-  getMenu();	
-
+  int num;
+  if (student[0].top == (MAXSIZE - 1))
+  {
+    printf ("Stack is Full\n");
     return;
+  }
+  else
+  {
+    
+    char nama [19];
+    int top;
+    top = student[0].top =  student[0].top + 1;
+    //gets(student[0].name);
+    printf("Input Rice type [ long / medium / short ]");
+    scanf("%s", student[top].name);
+    while (strcmp (student[top].name,"long")  != 0  && strcmp (student[top].name,"medium")  != 0  && strcmp (student[top].name,"short")  != 0  ){
+      printf("Input Rice type [ long / medium / short ]");
+      scanf("%s",student[top].name);
+    }
+    printf ("Input weight of the rice sack [10.. 100 kg<s> ] :\n");
+    scanf ("%d", &num);
+    while(num>= 100 || num<=10){
+      printf ("Input weight of the rice sack [10.. 100 kg<s> ] :\n");
+      scanf ("%d", &num);
+    }
+    student[0].stk[student[0].top] = num;
+  }
+  system("pause");
+  getMenu();	
+  
 }
 void initCake(){
-	
-//  strcpy(Products[0].name,"Blueberry Cake");
-  s.stock[0]=10;
-//  strcpy(Products[1].name,"Chocochip Cake");
-  s.stock[1]=5;
-//  strcpy(Products[2].name,"Mayoinase Cake");
- s.stock[2]=12;
-//  strcpy(Products[3].name,"Strowberry ShortCake");
- s.stock[3]=7;
+  // strcpy(student[0].name ,"Kaka")	;
+  // student[0].stk[0]=12;
 }
 void enter(int x){
   for(int i=0; i<x;i++){
@@ -97,58 +83,43 @@ void getMenu(){
   
 }
 void stockRice(){
-//  printf("--------------------------------------------------------------\n");
-//  
-//  if (sa.stock == -1 ){
-//    printf ("Stack is empty\n");
-//    return;
-//  }
-//  else {
-//    for (int i =  0; i <= 8; i++){
-//      
-//      if(i == 0){
-//        printf("%d. : %29s | %-12d |", i, Products[0].name, Products[0].stock);
-//        printf("top\n");
-//      }
-//      
-//      printf("%d. : %29s | %-12d | \n", i+1, Products[i+1].name, Products[i+1].stock);
-//    }
-//    printf("--------------------------------------------------------------\n");
-//    
-//  }
-int i;
-    if (s.top == -1)
-    {
-        printf ("Stack is empty\n");
-        return;
+  int i;
+  if ( student[0].top == -1)
+  {
+    printf ("Stack is empty\n");
+    return;
+  }
+  else
+  {
+    printf ("\n The status of the stack is \n");
+    if ( student[0].top == student[0].top){
+      printf ("%s %d ",  student[student[0].top].name, student[0].stk[student[0].top] );
+      printf("top\n");
     }
-    else
+    for (i =  student[0].top; i >= 1; i--)
     {
-        printf ("\n The status of the stack is \n");
-        for (i = s.top; i >= 0; i--)
-        {
-            printf ("%d %s \n", s.stock[i], s.name[i]);
-        }
+      printf ("%s %d \n",  student[i-1].name, student[0].stk[i-1] );
+      
     }
-    printf ("\n");
+  }
+  printf ("\n");
 }
-
 int sellRice(){
-    int num;
-    if (s.top == - 1)
-    {
-        printf ("Stack is Empty\n");
-        return (s.top);
-    }
-    else
-    {
-        num = s.stock[s.top];
-        printf ("poped element is = %dn", s.stock[s.top]);
-        s.top = s.top - 1;
-    }
+  int num;
+  if ( student[0].top == - 1)
+  {
+    printf ("Stack is Empty\n");
+    return ( student[0].top);
+  }
+  else
+  {
+    num =  student[0].stk[ student[0].top];
+    printf ("poped element is = %dn",  student[0].stk[ student[0].top]);
+    student[0].top =  student[0].top - 1;
+  }
   system("pause");
   getMenu();	
-
+  
   return(num);
-
+  
 }
